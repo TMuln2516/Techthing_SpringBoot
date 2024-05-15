6abdd83b76a9f1ee7f0040270f416979bad060a3
TMuln2516 <qhuynehihi@gmail.com>
2024-05-15 16:27:37 +0700

    update cart, cartdetail

--- src/main/java/com/example/techthing/entity/Cart.java
+++ src/main/java/com/example/techthing/entity/Cart.java
@@ -3,12 +3,9 @@ package com.example.techthing.entity;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,23 +22,13 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Product {
+public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
-    String name;
-    int quantity;
-    double price;
-    String image;
-    String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    Category category;
+    // User user;
 
-    @OneToMany(mappedBy = "product")
-    Set<InvoiceDetail> InvoiceDetails;
-
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "cart")
     Set<CartDetail> cartDetails;
 }
