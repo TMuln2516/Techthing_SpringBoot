ccd634fc0c5c6b45c26a72d58156079a082d0b85
tn9th8 <tn9th8@gmail.com>
2024-05-19 20:08:15 +0700

    base role, user

--- src/main/java/com/example/techthing/configruation/ApplicationInitConfig.java
+++ src/main/java/com/example/techthing/configruation/ApplicationInitConfig.java
@@ -1,65 +1,29 @@
 package com.example.techthing.configruation;
 
-import com.example.techthing.entity.Role;
-import com.example.techthing.entity.User;
-import com.example.techthing.repository.RoleRepository;
-import com.example.techthing.repository.UserRepository;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import java.util.HashSet;
+
+import com.example.techthing.database.RoleData;
+import com.example.techthing.database.UserData;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Configuration
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ApplicationInitConfig {
-
-    PasswordEncoder passwordEncoder;
-    RoleRepository roleRepository;
+    RoleData roleData;
+    UserData userData;
 
     @Bean
-    ApplicationRunner applicationRunner(UserRepository userRepository) {
+    ApplicationRunner applicationRunner() {
         return args -> {
-            if (roleRepository.findById("ADMIN").isEmpty()) {
-                Role role = Role.builder()
-                        .name("ADMIN")
-                        .description("Role Admin")
-                        .build();
-                roleRepository.save(role);
-            }
+            this.roleData.createBase();
+            this.userData.createBase();
 
-            if (roleRepository.findById("MANAGER").isEmpty()) {
-                Role role = Role.builder()
-                        .name("MANAGER")
-                        .description("Role Manager")
-                        .build();
-                roleRepository.save(role);
-            }
-
-            if (roleRepository.findById("USER").isEmpty()) {
-                Role role = Role.builder()
-                        .name("USER")
-                        .description("Role User")
-                        .build();
-                roleRepository.save(role);
-            }
-
-            if (userRepository.findByUsername("admin").isEmpty()) {
-                HashSet<Role> roles = new HashSet<>();
-                roles.add(roleRepository.findById("ADMIN").orElseThrow());
-                User admin = User.builder()
-                        .username("admin")
-                        .password(passwordEncoder.encode("admin"))
-                        .roles(roles)
-                        .address("")
-                        .build();
-
-                userRepository.save(admin);
-            }
         };
     }
 }
